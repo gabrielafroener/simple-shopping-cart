948a60e07133c2030cfffac0d11dcdbff8943386
Gabriela <gabi@aeroli.to>
2019-12-20 16:01:18 -0300

    Add fake API

---
+++ src/api.js
@@ -0,0 +1,232 @@
+export const products = [
+  {
+    id: 12,
+    title: "Cat Tee Black T-Shirt",
+    price: 10.9,
+    installments: 9,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/12/picsum/50",
+    idCategory: "0"
+  },
+
+  {
+    id: 13,
+    title: "Dark Thug Blue-Navy T-Shirt",
+    price: 29.45,
+    installments: 5,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/13/picsum/50",
+    idCategory: "0"
+  },
+
+  {
+    id: 14,
+    title: "Sphynx Tie Dye Wine T-Shirt",
+    price: 9.0,
+    installments: 3,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/14/picsum/50",
+    idCategory: "2"
+  },
+
+  {
+    id: 15,
+    title: "Skuul",
+    price: 14.0,
+    installments: 5,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/15/picsum/50",
+    idCategory: "1"
+  },
+
+  {
+    id: 11,
+    title: "Wine Skul T-Shirt",
+    price: 13.25,
+    installments: 3,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/11/picsum/50",
+    idCategory: "2"
+  },
+
+  {
+    id: 16,
+    title: "Short Sleeve T-Shirt",
+    price: 75.0,
+    installments: 5,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/16/picsum/50",
+    idCategory: "0"
+  },
+
+  {
+    id: 0,
+    title: "Cat Tee Black T-Shirt",
+    price: 10.9,
+    installments: 9,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/0/picsum/50",
+    idCategory: "1"
+  },
+
+  {
+    id: 1,
+    title: "Sphynx Tie Dye Grey T-Shirt",
+    price: 10.9,
+    installments: 9,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/1/picsum/50",
+    idCategory: "2"
+  },
+
+  {
+    id: 2,
+    title: "Danger Knife Grey",
+    price: 14.9,
+    installments: 7,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/2/picsum/50",
+    idCategory: "1"
+  },
+
+  {
+    id: 3,
+    title: "White DGK Script Tee",
+    price: 14.9,
+    installments: 7,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/3/picsum/50",
+    idCategory: "1"
+  },
+
+  {
+    id: 4,
+    title: "Born On The Streets",
+    price: 25.9,
+    installments: 12,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: false,
+    image: "https://picsum.photos/4/picsum/50",
+    idCategory: "0"
+  },
+
+  {
+    id: 5,
+    title: "Tso 3D Short Sleeve T-Shirt A",
+    price: 10.9,
+    installments: 9,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: false,
+    image: "https://picsum.photos/5/picsum/50",
+    idCategory: "0"
+  },
+
+  {
+    id: 6,
+    title: "Man Tie Dye Cinza Grey T-Shirt",
+    price: 49.9,
+    installments: 0,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/6/picsum/50",
+    idCategory: "2"
+  },
+
+  {
+    id: 7,
+    title: "Crazy Monkey Black T-Shirt",
+    price: 22.5,
+    installments: 4,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/7/picsum/50",
+    idCategory: "1"
+  },
+
+  {
+    id: 8,
+    title: "Tso 3D Black T-Shirt",
+    price: 18.7,
+    installments: 4,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: false,
+    image: "https://picsum.photos/8/picsum/50",
+    idCategory: "2"
+  },
+
+  {
+    id: 9,
+    title: "Crazy Monkey Grey",
+    price: 134.9,
+    installments: 5,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/9/picsum/50",
+    idCategory: "0"
+  },
+
+  {
+    id: 10,
+    title: "On The Streets Black T-Shirt",
+    price: 49.0,
+    installments: 9,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/10/picsum/50",
+    idCategory: "1"
+  },
+
+  {
+    id: 17,
+    title: "Pillow dummy test",
+    price: 49.0,
+    installments: 9,
+    currencyId: "USD",
+    currencyFormat: "$",
+    isFreeShipping: true,
+    image: "https://picsum.photos/17/picsum/50",
+    idCategory: "2"
+  }
+];
+
+export const categories = [
+  {
+    id: 0,
+    title: "Cool"
+  },
+  {
+    id: 1,
+    title: "Dope"
+  },
+  {
+    id: 2,
+    title: "Ok"
+  }
+];
