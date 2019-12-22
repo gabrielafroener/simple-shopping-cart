73da45b475957f2f26042ee3433d3bf736789217
Gabriela <gabi@aeroli.to>
2019-12-22 09:55:39 -0300

    Add products list with category selection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { List, Select, Button } from "antd";
 import Layout from "../components/Layout";
+import { products, categories } from "../api";
+import "./styles/index.scss";
 
-export default () => (
-  <Layout>
-    <div>
-      <h1>Home</h1>
-    </div>
-  </Layout>
-);
+const { Option } = Select;
+
+export default () => {
+  const [productsList, setProductsList] = useState(products);
+
+  const handleChange = value => {
+    value === "All"
+      ? setProductsList(products)
+      : setProductsList(products.filter(p => p.idCategory === value));
+  };
+
+  return (
+    <Layout>
+      <div className="home">
+        <Select className="select" defaultValue="All" onChange={handleChange}>
+          {categories.map(category => (
+            <Option value={category.id}>{category.title}</Option>
+          ))}
+          <Option value="All">All</Option>
+        </Select>
+
+        <List
+          className="list"
+          bordered
+          dataSource={productsList}
+          pagination={true}
+          renderItem={item =>
+            item &&
+            item.title && (
+              <List.Item className="list-item">
+                {item.title}
+                <Button type="primary" className="button">
+                  Add to cart
+                </Button>
+              </List.Item>
+            )
+          }
+        />
+      </div>
+    </Layout>
+  );
+};
